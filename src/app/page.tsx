5fade2d14aee75a95f48a659c4ea0ead7d416c4b
XenoZ <xenoz0096@gmail.com>
2024-06-05 16:13:06 +0530

    tt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import { Auth0Provider } from '@auth0/auth0-react';
@@ -12,27 +11,37 @@ import {
 } from '@/containers';
 
 import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 
 const Home: NextPage = () => {
+  const [redirectUri, setRedirectUri] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setRedirectUri(window.location.origin);
+    }
+  }, []);
+
+  if (!redirectUri) {
+    return null; // Prevents rendering until redirectUri is set
+  }
+
   return (
-    <>
-      <Auth0Provider
-        domain="dev-7hl8rw07vhindcpd.us.auth0.com"
-        clientId="LglRoyCgeMjFeBWZoOBk72Ric4H1u5jQ"
-        authorizationParams={{
-          redirect_uri: window.location.origin
-        }}
-      >
-        <Layout>
-          
-          <Hero />
-          <About />
-          <Skills />
-          <Projects />
-          <Contact />
-        </Layout>
-      </Auth0Provider>
-    </>
+    <Auth0Provider
+      domain="dev-7hl8rw07vhindcpd.us.auth0.com"
+      clientId="LglRoyCgeMjFeBWZoOBk72Ric4H1u5jQ"
+      authorizationParams={{
+        redirect_uri: redirectUri
+      }}
+    >
+      <Layout>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </Layout>
+    </Auth0Provider>
   );
 };
 
