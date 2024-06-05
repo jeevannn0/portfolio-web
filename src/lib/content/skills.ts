c8f74fe96ec629d6836258f17aa4e75e90bc4939
XenoZ <xenoz0096@gmail.com>
2024-06-05 15:37:05 +0530

    tt

--- src/lib/content/skills.ts
+++ src/lib/content/skills.ts
@@ -9,52 +9,34 @@ export const skillsSection: SkillsSectionType = {
         title: 'full stack development',
         // animation lottie file: https://lottiefiles.com/
         lottie: {
-        light: '/lotties/frontend.json',
+        light: '/lotties/frontend-dark.json',
         dark: '/lotties/frontend-dark.json',
         },
         points: [
-        'Building responsive static websites using Next.js',
-        'Building responsive Single Page Apps in React.js',
-        'Building RESTful APIs in Express',
-        ],
-        softwareSkills: [
-          // iconify icons: https://icon-sets.iconify.design/
-        { name: 'html-5', icon: 'vscode-icons:file-type-html' },
-        { name: 'CSS-3', icon: 'vscode-icons:file-type-css' },
-        { name: 'sass', icon: 'vscode-icons:file-type-sass' },
-        { name: 'javaScript', icon: 'vscode-icons:file-type-js-official' },
-        {
-            name: 'typeScript',
-            icon: 'vscode-icons:file-type-typescript-official',
-        },
-        { name: 'python', icon: 'vscode-icons:file-type-python' },
-        { name: 'nodejs', icon: 'logos:nodejs-icon' },
-        { name: 'reactjs', icon: 'logos:react' },
-        { name: 'nextjs', icon: 'logos:nextjs-icon' },
-          // { name: 'angularjs', icon: 'logos:angular-icon' },
-        { name: 'tailwindcss', icon: 'logos:tailwindcss-icon' },
-        { name: 'redux', icon: 'logos:redux' },
-        { name: 'database', icon: 'vscode-icons:file-type-sql' },
-        { name: 'jest', icon: 'vscode-icons:file-type-jest' },
-        ],
-    },
-    {
-        id: getId(),
-        title: 'UI/UX designing',
-        lottie: {
-        light: '/lotties/designing.json',
-        dark: '/lotties/designing-dark.json',
-        },
-        points: [
-        'Experience in designing user-friendly interfaces with figma',
-        'Experience in developing design systems and style guides',
-        'Providing user-friendly design solutions',
-        ],
-        softwareSkills: [
-        { name: 'figma', icon: 'logos:figma' },
-        { name: 'adobe illustrator', icon: 'logos:adobe-illustrator' },
-        { name: 'adobe photoshop', icon: 'logos:adobe-photoshop' },
-        ],
-    },
-    ],
-};
+          'Building responsive Single Page Apps in React.js',
+          'Building responsive static websites using Next.js',
+          'Creating Attractive UI designs in Figma',
+          ],
+          softwareSkills: [
+            // iconify icons: https://icon-sets.iconify.design/
+            
+          { name: 'html-5', icon: 'vscode-icons:file-type-html' },
+          { name: 'CSS-3', icon: 'vscode-icons:file-type-css' },
+          { name: 'javaScript', icon: 'vscode-icons:file-type-js-official' },
+          {
+              name: 'typeScript',
+              icon: 'vscode-icons:file-type-typescript-official',
+          },
+          { name: 'tailwindcss', icon: 'logos:tailwindcss-icon' },
+          { name: 'reactjs', icon: 'logos:react' },
+          { name: 'redux', icon: 'logos:redux' },
+          { name: 'nextjs', icon: 'logos:nextjs-icon' },
+          { name: 'nodejs', icon: 'logos:nodejs-icon' },
+          { name: 'superbase', icon: "skill-icons:supabase-dark" },
+          { name: 'firebase', icon: "logos:firebase" },
+          { name: 'mongodb', icon: "devicon:mongodb" },
+          ],
+      },
+      ],
+  };
+  
