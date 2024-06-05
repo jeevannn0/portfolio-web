9faa0a8ad70858c58db054377675e93246bdb8ba
XenoZ <xenoz0096@gmail.com>
2024-06-05 15:20:54 +0530

    tt

---
+++ src/lib/content/portfolio.ts
@@ -0,0 +1,38 @@
+/*
+ * =========================
+ * AUTHOR INFO
+ * =========================
+ */
+
+import { StringKeyValueType } from '../types';
+
+export const socialLinks: StringKeyValueType = {
+  instagram: 'https://www.instagram.com/j___v_n',
+  twitter: 'https://twitter.com/jeevannn0',
+  github: 'https://github.com/jeevannn0',
+  linkedin: 'https://www.linkedin.com/in/jeevannn',
+};
+
+export const author = {
+  name: 'Portfolio',
+  email: 'jeevannn0@gmail.com',
+};
+
+export const seoData = {
+  title: 'Jeevan | Front-End Developer',
+  description:
+    'Jeevan is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
+  author: author.name,
+  image:
+    '',
+  url: 'https://jeevannn0.vercel.app/',
+  keywords: [
+    'Jeevan',
+    'Jeevan',
+    '@jeevannn0',
+    'jeevannn0',
+    'Portfolio',
+    'Jeevan Portfolio ',
+    'Jeevan Portfolio',
+  ],
+};
