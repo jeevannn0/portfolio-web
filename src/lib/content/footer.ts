86081ac9d1de6f18b0fc055564b3c9a3d373c62b
XenoZ <xenoz0096@gmail.com>
2024-06-05 17:22:18 +0530

    link

--- src/lib/content/footer.ts
+++ src/lib/content/footer.ts
@@ -2,5 +2,5 @@ import { FooterSectionType } from '@/lib/types/sections';
 
 export const footerSection: FooterSectionType = {
     title: 'Design & Built by jeevan',
-    link: '',
+    link: 'https://jeevan-portfolio-ts.vercel.app/',
 };
