8acc64dc9f96346668f91a0c3db3423a320a6300
XenoZ <xenoz0096@gmail.com>
2024-06-06 09:53:48 +0530

    not found pg

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,10 +5,7 @@ const NotFound = () => {
   return (
     <Layout className="grid h-screen place-items-center">
       <div className="w-full max-w-xl text-center">
-      <img src="https://media1.tenor.com/m/_DOAnKatq7EAAAAd/cod-chost.gif"
-          alt="Ghost GIF"
-          className="mx-auto h-96"/>
-
+      
         <p className="mt-5 text-3xl capitalize md:text-4xl text-dark-2">
           Sus, Entered the wrong page
         </p>
