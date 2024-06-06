c2776735f37555cf15bef851f548c567dce43472
XenoZ <xenoz0096@gmail.com>
2024-06-06 09:51:06 +0530

    not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,16 @@
-import { Button, ShowLottie } from '@/components';
+import { Button } from '@/components';
 import { Layout } from '@/containers';
 
 const NotFound = () => {
   return (
     <Layout className="grid h-screen place-items-center">
       <div className="w-full max-w-xl text-center">
-        <ShowLottie path="/lotties/404.json" className="mx-auto" />
+      <img src="https://media1.tenor.com/m/_DOAnKatq7EAAAAd/cod-chost.gif"
+          alt="Ghost GIF"
+          className="mx-auto h-96"/>
 
         <p className="mt-5 text-3xl capitalize md:text-4xl text-dark-2">
-          page not found
+          Sus, Entered the wrong page
         </p>
 
         <Button type="link" href="/" size="lg" className="mt-20" sameTab center>
